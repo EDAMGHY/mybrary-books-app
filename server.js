6ec9858c464bcdf11faa1334f0700f296c0bcff4
Abdellah DAMRI <damriabdellah@gmail.com>
2022-03-18 16:09:22 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDB = require('./db');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+require('dotenv').config();
+
+connectDB();
+// template engine ejs config
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+
+// images css js files
+app.use(express.static('public'));
+
+// routes
+app.use(require('./routes'));
+
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT, () => console.log(`Server Listening on PORT ${PORT}...`));
